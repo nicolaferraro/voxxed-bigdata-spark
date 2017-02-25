30b93c2fdd03ec570a0aa4751c29db10dd206b15
Nicola Ferraro <ni.ferraro@gmail.com>
2017-02-25 20:18:46 +0100

    Spark streaming app

---
+++ readme.md
@@ -0,0 +1,8 @@
+# Spark Kubernetes Quickstart
+
+## Requirements
+
+- Openshift origin (`oc cluster up`)
+- http://search.maven.org/remotecontent?filepath=io/fabric8/ipaas/apps/kafka/2.2.168/kafka-2.2.168-openshift.yml
+- http://search.maven.org/remotecontent?filepath=io/fabric8/ipaas/apps/zookeeper/2.2.168/zookeeper-2.2.168-openshift.yml
+- http://radanalytics.io/resources.yaml (install web ui)
--- src/main/scala/spark/kubernetes/quickstart/Hello.scala
+++ src/main/scala/spark/kubernetes/quickstart/Hello.scala
@@ -1,5 +1,10 @@
 package spark.kubernetes.quickstart
 
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
+import org.apache.spark.streaming.kafka010.KafkaUtils
+import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
+import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.spark.{SparkConf, SparkContext}
 
 /**
@@ -11,9 +16,30 @@ object Hello {
 
     val conf = new SparkConf().setAppName("Hello")
     val sc = new SparkContext(conf)
+    val streamingContext = new StreamingContext(sc, Seconds(10))
 
-    val result = sc.parallelize(1 to 100).reduce(_ + _)
-    println(result)
+
+    val kafkaParams = Map[String, Object](
+      "bootstrap.servers" -> "kafka:9092",
+      "key.deserializer" -> classOf[StringDeserializer],
+      "value.deserializer" -> classOf[StringDeserializer],
+      "group.id" -> "spark",
+      "auto.offset.reset" -> "latest",
+      "enable.auto.commit" -> (false: java.lang.Boolean)
+    )
+
+    val topics = Array("stars")
+    val stream = KafkaUtils.createDirectStream[String, String](
+      streamingContext,
+      PreferConsistent,
+      Subscribe[String, String](topics, kafkaParams)
+    )
+
+    stream.map(record => (record.key(), record.value())).foreachRDD(rdd => rdd.foreach(println))
+
+    streamingContext.start()
+    streamingContext.awaitTermination()
+    sc.stop()
   }
 
 }
