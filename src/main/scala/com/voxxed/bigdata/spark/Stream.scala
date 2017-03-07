a41dda76090383d4e35f617d2b2bb63a5dccf0e2
Nicola Ferraro <ni.ferraro@gmail.com>
2017-03-07 02:34:56 +0100

    First working version

--- src/main/scala/com/voxxed/bigdata/spark/Stream.scala
+++ src/main/scala/com/voxxed/bigdata/spark/Stream.scala
@@ -15,51 +15,40 @@ object Stream {
     val sc = new SparkContext(conf)
     val ssc = new StreamingContext(sc, Seconds(10))
 
-    val watchWindow = Minutes(5)
+    val watchWindow = Minutes(10)
     val topK = 10
 
-    ssc.checkpoint("/tmp/checkpoint")
+//    val source = ssc.socketTextStream("localhost", 9999)
+//      .map(Event.fromJson)
 
-    def updateFunction(newValues: Seq[RatingInfo], runningCount: Option[RatingInfo]): Option[RatingInfo] = {
-      val curCount = runningCount.map(r => r.count).getOrElse(0L)
-      val curSum = runningCount.map(r => r.sum).getOrElse(BigDecimal(0))
-      val newCount = newValues.map(r => r.count).fold(curCount)(_ + _)
-      val newSum = newValues.map(r => r.sum).fold(curSum)(_ + _)
-
-      val result = runningCount.orElse(newValues.headOption).map(r => r.copy(count = newCount, sum = newSum))
-      result
-    }
-
-//        val events = ssc.socketTextStream("localhost", 9999)
-//          .map(Event.fromJson)
-
-    val events = KafkaUtils.createDirectStream[String, String](
+    val source = KafkaUtils.createDirectStream[String, String](
       ssc,
       PreferConsistent,
       Subscribe[String, String](Seq("stars"), KafkaSupport.kafkaParams)
     ).map(record => Event.fromJson(record.value()))
 
+    val events = source.window(watchWindow)
+      .map(e => ((e.userId, e.itemId), e))
+      .groupByKey()
+      .flatMap({case ((user, item), events) => events.filter(e => !events.exists(e2 => e2.timestamp > e.timestamp))})
+      .cache()
+
     val state = events
       .map(e => (e.userId, e))
       .groupByKey()
-      .map({ case (user, items) => items.toList.sortBy(_.timestamp) })
-      .flatMap(list => list.zip(list.drop(1)).map({ case (ev1, ev2) => (ev1.itemId, ev2.itemId, BigDecimal(ev1.rating) - BigDecimal(ev2.rating)) }))
-      .map({ case (item1, item2, diff) => ((item1, item2), diff) })
+      .map({ case (user, items) => items.toList })
+      .flatMap(list => {
+        val prod = for(x <- list; y <- list; if (x != y)) yield (x, y)
+        prod.map({ case (ev1, ev2) => ((ev1.itemId, ev2.itemId), BigDecimal(ev1.rating) - BigDecimal(ev2.rating)) })
+      })
       .groupByKey()
       .mapValues(rat => (rat.size, rat.sum))
       .map({ case ((item1, item2), (size, sum)) => RatingInfo(item1, item2, size, sum) })
-      .flatMap(rating => Seq(rating, rating.copy(item1 = rating.item2, item2 = rating.item1, sum = -rating.sum)))
       .map(r => ((r.item1, r.item2), r))
-      .updateStateByKey(updateFunction)
       .map({ case ((item1, item2), rating) => (item1, rating) })
 
 
-    val latestEvents = events.window(watchWindow)
-      .map(e => ((e.userId, e.itemId), e))
-      .groupByKey()
-      .flatMap({ case (userItem, evts) => evts.filter(e => !evts.exists(e2 => e2.timestamp > e.timestamp)) })
-
-    val recommendations = latestEvents
+    val recommendations = events
       .map(event => (event.itemId, event))
       .join(state)
       .map({ case (itemId, (event, ratingInfo)) =>
@@ -70,7 +59,7 @@ object Stream {
       .map({ case ((user, item), rating) => (user, (item, rating)) })
 
 
-    val filteredRecommendations = latestEvents.map(e => (e.userId, e))
+    val filteredRecommendations = events.map(e => (e.userId, e))
       .cogroup(recommendations)
       .flatMap({ case (user, (watchedMovies, suggestions)) =>
         val watched = watchedMovies.map(e => e.itemId).toSet
@@ -83,8 +72,8 @@ object Stream {
       .mapValues(sugg => sugg.toList.sortBy({ case (movie, prediction) => -prediction }).map(_._1).take(topK))
       .map({ case (user, items) => Recommendation(user, items) })
 
-//    topByUser.foreachRDD(rdd => rdd.foreach(recomm => KafkaSupport.send("recommendations", recomm.userId.toString, recomm.toJson)))
-    topByUser.foreachRDD(rdd => rdd.foreach(recomm => println(recomm.toJson)))
+    topByUser.foreachRDD(rdd => rdd.foreach(recomm => KafkaSupport.send("recommendations", recomm.userId.toString, recomm.toJson)))
+//    topByUser.print()
 
     ssc.start()
     ssc.awaitTermination()
